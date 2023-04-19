cdaf0cfd64eff73199f65c0bf3f2d507e6345c75
Kristina Boyarskaya <Kristina-Snegurova@ya.ru>
2023-04-19 12:34:15 +0300

    add Home_Task_2

---
+++ Home_Task_2/Task_0.java
@@ -0,0 +1,35 @@
+// 0. Посчитайте сколько драгоценных камней в куче обычных камней
+// Пример:
+// jewels = “aB”, stones = “aaaAbbbB”
+// Результат в консоль ”a3B1”
+// public static void main(String[] args) {
+
+// }
+
+package Home_Task_2;
+import java.util.Scanner;
+public class Task_0 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Введите названия драгоценных камней =  ");
+    String jewels = sc.nextLine();
+    System.out.print("Перечислите все камни в куче =  ");
+    String stones = sc.nextLine();
+    String res = findjewelsInStones(jewels, stones);
+    System.out.println(res);
+    sc.close();
+    }
+    public static String findjewelsInStones(String jewels,String stones){
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < jewels.length(); i++){
+            int count = 0;
+            for(int j = 0; j < stones.length(); j++){
+                if(jewels.charAt(i) == stones.charAt(j)){
+                    count++;
+                }
+            }
+            res.append(jewels.charAt(i)).append(count);
+        }
+        return res.toString();
+    }
+}
